c1ce38d73ac51b4880614690d5f22bd0deafb8e0
katarina6727 <kmthompson460@gmail.com>
2023-07-12 08:17:52 -0400

    Day 4 Puzzle 1 works

---
+++ Day_4/Day_4_Puzzle_1.py
@@ -0,0 +1,72 @@
+# initialize variables
+passportData = ""
+dataList = []
+validPassports = 0
+valid = True
+
+# open the file with all of the passport information and assign it to f
+f = open('passport_input.txt')
+
+
+for line in f.readlines():
+    # keep adding the same passport data to the passportData variable until there is a line break indicating the following data would
+    # be for a different passport
+    if (line != "\n"):
+        passportData = passportData + line
+    if (line == "\n"):
+        # if there are any separate lines in the passport data, replace them with a space
+        if (passportData.index("\n") != -1):
+            passportData = passportData.replace("\n", " ")
+        # separate each piece of data in a list by finding the spaces between pieces of data
+        while (passportData.find(" ") != -1):
+            index = passportData.find(" ")
+            dataList.append(passportData[:index])
+            passportData = passportData[index+1:]
+        # add to the valid passport count if a passport has all 8 required fields
+        length = len(dataList)
+        if (length > 7):
+            validPassports += 1
+        # if the passport only has 7 of the 8 required fields determine if the missing field is the country ID. If it is, add to the
+        # valid passport count
+        elif (length == 7):
+            for i in range(7):
+                dataPiece = dataList[i]
+                if (dataPiece.find("cid") != -1):
+                    valid = False
+            if valid:
+                validPassports += 1
+
+        # reset the current passport's data, data list, and valid variable for the next passport
+        valid = True
+        passportData = ""
+        dataList = []
+        
+f.close()
+
+# run the code one more time with some modifications for the last passport
+# if there are any separate lines in the passport data, replace them with a space
+if (passportData.index("\n") != -1):
+    passportData = passportData.replace("\n", " ")
+# separate each piece of data into a list by finding the spaces between pieces of data
+while (passportData.find(" ") != -1):
+    index = passportData.find(" ")
+    dataList.append(passportData[:index])
+    passportData = passportData[index+1:]
+if (passportData != ""):
+    dataList.append(passportData)
+# add to the valid passport count if the passport has all 8 required fields
+length = len(dataList)
+if (length > 7):
+    validPassports += 1
+# if the passport only has 7 of the 8 required fields determine if the missing field is the country ID. If it is, add to the
+# valid passport count
+elif (length == 7):
+    for i in range(7):
+        dataPiece = dataList[i]
+        if (dataPiece.find("cid") != -1):
+            valid = False
+    if valid:
+        validPassports += 1
+
+# print the total number of valid passports
+print("There are", validPassports, "valid passports")
